f2051042288ba41d0a164e6b4bffa69f9db7dabe
Rizal Hidayat <realrizalhidayat@gmail.com>
2022-01-04 09:01:15 +0700

    init project

---
+++ SJS+ Employee/SJS+ Employee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SJS+ Employee
+//
+//  Created by Rizal Hidayat on 04/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

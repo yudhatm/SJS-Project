d3ae157aaac5aca74e9f52cfbe3edc8deacdbaf8
Rizal Hidayat <realrizalhidayat@gmail.com>
2022-04-18 22:46:09 +0700

    add sign in view

---
+++ SJS+ Employee/Features/Authentication/SignIn/Views/AuthenticationSignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationSignInViewController.swift
+//  SJS+ Employee
+//
+//  Created by Rizal Hidayat on 18/04/22.
+//
+
+import UIKit
+
+class AuthenticationSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

0e90d3680c28e4c80ddda714a89929fd5fe6146a
Prabaesa Yudha Tama <prabaesayudhatama@gmail.com>
2022-05-10 18:37:07 +0700

    profile UI in progress

---
+++ SJS+ Employee/ViewControllers/Profile/Cells/ProfileListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileListTableViewCell.swift
+//  SJS+ Employee
+//
+//  Created by Buana on 10/05/22.
+//
+
+import UIKit
+
+class ProfileListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SJS+ Employee/ViewControllers/Profile/Cells/ProfileVerificationCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileVerificationCollectionViewCell.swift
+//  SJS+ Employee
+//
+//  Created by Buana on 10/05/22.
+//
+
+import UIKit
+
+class ProfileVerificationCollectionViewCell: UICollectionViewCell {
+    
+}
